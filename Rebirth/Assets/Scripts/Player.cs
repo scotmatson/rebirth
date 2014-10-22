5ceebd43c767c4918ad88240ed1637a56ed9f438
Joseph ORLANDO <josephorlando@Josephs-MacBook-Pro.local>
2014-10-21 23:09:00 -0700

    Initial Commit I just copied my stuff form other repo

---
+++ Rebirth/Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Player : MonoBehaviour
+    {
+        public float PlayerMovementSpeed;
+
+        private Animator _anim;
+        private CharacterController _cont;
+
+        // Use this for initialization
+        void Start ()
+        {
+            _anim = GetComponent<Animator>();
+            _cont = GetComponent<CharacterController>();
+        }
+	
+        // Update is called once per frame
+        void Update () {
+
+            var deltaX = Input.GetAxis( "Horizontal" ) * PlayerMovementSpeed;
+            var deltaY = Input.GetAxis( "Vertical" ) * PlayerMovementSpeed;
+
+            var newPosition = new Vector3(deltaX,deltaY,0);
+
+            _cont.Move( newPosition * Time.deltaTime );
+        }
+    }
+}
