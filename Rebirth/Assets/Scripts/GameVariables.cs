65d6ddc62f1b490a1c4713455c1ffcb7467f8845
Scot Matson <sjpmatson@gmail.com>
2014-10-22 17:47:12 -0700

    Add script GameVariable for static globals. Add script PlayerHealth for health/timer logic.

---
+++ Rebirth/Assets/Scripts/GameVariables.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class GameVariables : MonoBehaviour {
+
+	public static float health = 1000;
+}
---
+++ Rebirth/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerHealth : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		// If the timer hasn't zero'd out and the player is still alive
+		// continue to count down.
+		if (GameVariables.health > 0) {
+			guiText.text = "Timer: " + GameVariables.timer;
+			GameVariables.timer -= Time.deltaTime;
+		}
+	}
+}
