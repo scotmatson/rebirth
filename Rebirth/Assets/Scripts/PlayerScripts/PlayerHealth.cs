6aaa7acbcde96e70c56c6e5dbafb5d4ed31c37de
Scot Matson <sjpmatson@gmail.com>
2014-10-26 11:23:24 -0700

    Truncated health on HUD. Displays whole numbers only.

--- Rebirth/Assets/Scripts/PlayerScripts/PlayerHealth.cs
+++ Rebirth/Assets/Scripts/PlayerScripts/PlayerHealth.cs
@@ -11,7 +11,7 @@ public class PlayerHealth : MonoBehaviour {
 		//If the timer hasn't zero'd out and the player is still alive
 		//continue to count down.
 		if (PlayerState.health > 0) {
-			guiText.text = "Health: " + PlayerState.health;
+			guiText.text = "Health: " + PlayerState.health.ToString("f0");
 			PlayerState.health -= Time.deltaTime;
 		}
 	}
