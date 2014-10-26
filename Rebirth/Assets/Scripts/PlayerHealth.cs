b84b13b6a434034a4587923ecf50fd4b87aaf707
Scot Matson <sjpmatson@gmail.com>
2014-10-25 22:44:02 -0700

    Made a public accessor to the script PlayerState from within HealthTimer. In the GUI I was able to couple the components. HealthTimer now works and player will die (destroy) when the timer runs out. Timer needs to be truncated to natural numbers still.

--- Rebirth/Assets/Scripts/PlayerHealth.cs
+++ Rebirth/Assets/Scripts/PlayerHealth.cs
@@ -3,20 +3,17 @@ using System.Collections;
 
 public class PlayerHealth : MonoBehaviour {
 
-	GameObject playerObj;
-	PlayerState player;
+	public PlayerState playerState;
 
 	void start () {
-		playerObj = GameObject.FindWithTag ("Player");
-		player = playerObj.GetComponent<PlayerState> ();
 	}
 	// Update is called once per frame
 	void Update () {
 		//If the timer hasn't zero'd out and the player is still alive
 		//continue to count down.
-		if (player.health > 0) {
-			guiText.text = "Health: " + player.health;
-			player.health -= Time.deltaTime;
+		if (playerState.health > 0) {
+			guiText.text = "Health: " + playerState.health;
+			playerState.health -= Time.deltaTime;
 		}
 	}
 }
