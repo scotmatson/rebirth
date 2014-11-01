ea6d1aff55531ba13b96d87915f8fff599fda8b4
frizzlefry22 <frizzlefry22@gmail.com>
2014-11-01 00:01:44 -0700

    Updated Player

--- Rebirth/Assets/Scripts/PlayerScripts/Player.cs
+++ Rebirth/Assets/Scripts/PlayerScripts/Player.cs
@@ -2,56 +2,84 @@
 
 
 public class Player : MonoBehaviour {
-	//float deltaX;
-	//float deltaY;
-	//float newPosition;
+
+
     public float PlayerMovementSpeed;
     private Animator _anim;
-    //private CharacterController _cont;
+    private CharacterController _cont;
+    private FacingDirection _direction;
 
     // Use this for initialization
     void Start () {
 		PlayerMovementSpeed = 3.0F;
     	_anim = GetComponent<Animator>();
-        //_cont = GetComponent<CharacterController>();
+        _cont = GetComponent<CharacterController>();
+        _direction = FacingDirection.RIGHT;
     }
 	
     // Update is called once per frame
    	void Update () {
-		move ();
-		attack ();
+		Move ();
+		Attack ();
     }
 
-	void move () {
-		//deltaX = Input.GetAxis( "Horizontal" ) * PlayerMovementSpeed;
-		//deltaY = Input.GetAxis( "Vertical" ) * PlayerMovementSpeed;
-		//newPosition = new Vector3(deltaX, deltaY, 0);
-		//_cont.Move( newPosition * Time.deltaTime );
-		float translateX = Input.GetAxis ("Horizontal") * PlayerMovementSpeed;
-		float translateY = Input.GetAxis ("Vertical") * PlayerMovementSpeed;
+	void Move () {
+
+		var deltaX = Input.GetAxis( "Horizontal" ) * PlayerMovementSpeed;
+		var deltaY = Input.GetAxis( "Vertical" ) * PlayerMovementSpeed;
+		var newPosition = new Vector3(deltaX, deltaY, 0);
+
+        //Move the Player using Character Controller Move to obey basic collsion
+        _cont.Move(newPosition * Time.deltaTime);
+        
+        UpdateDirection(deltaX,deltaY);
 		
-		//Run left and right
-		if (translateX != 0) {
-			translateY = 0;
-			translateX *= Time.deltaTime;
-			transform.Translate (translateX, 0f, 0f);
-		}
+        _anim.SetFloat ("moveX", deltaX);
 
-		//Run up and down
-		if (translateY != 0) {
-			translateX = 0;
-			translateY *= Time.deltaTime;
-			transform.Translate (0f, translateY, 0f);
-		}
+        //Prvents animation problem with moving diagnol by animiation going sideways but up or down
+	    if (deltaX != 0f)
+	    {
+	        deltaY = 0f;
+	    }
+        _anim.SetFloat("moveY", deltaY);
 
-		_anim.SetFloat ("moveX", translateX);
-		_anim.SetFloat ("moveY", translateY);
+    }
+
+	void Attack ()
+	{
+	    var attacking = Input.GetKeyDown(KeyCode.Mouse0);
+	    _anim.SetBool("attacking", attacking);
+
+
 	}
 
-	void attack () {
-		bool attacking = false;
-		attacking = (Input.GetAxis ("Fire1") == 1) ? true : false;
-		_anim.SetBool ("attacking", attacking);
-	}	
+    /// <summary>
+    /// Method that updates the Facing Direction to be used for Firing
+    /// </summary>
+    /// <param name="deltaX"></param>
+    /// <param name="deltaY"></param>
+    void UpdateDirection(float deltaX, float deltaY)
+    {
+        if (deltaX > 0)
+        {
+            _direction = FacingDirection.RIGHT;
+        }
+        else if (deltaX < 0)
+        {
+            _direction = FacingDirection.LEFT;
+        }
+        else if (deltaY > 0)
+        {
+            _direction = FacingDirection.UP;
+        }
+        else if (deltaY < 0)
+        {
+            _direction = FacingDirection.DOWN;
+        }
+    }
+
+
 }
+
+public enum FacingDirection { LEFT = 1, RIGHT = 2, UP = 3, DOWN = 4 };
 
