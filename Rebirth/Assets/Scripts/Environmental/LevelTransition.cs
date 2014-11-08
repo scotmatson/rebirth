028ac300dd47895e9b7ab6ff5d2d0a36bf7b24ff
frizzlefry22 <frizzlefry22@gmail.com>
2014-11-08 10:56:47 -0800

    Added stuff to Level1.unity Added LevelTransition.cs that lets an object move player to next level on Trigger

---
+++ Rebirth/Assets/Scripts/Environmental/LevelTransition.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelTransition : MonoBehaviour {
+
+
+    void OnTriggerEnter(Collider col)
+    {
+
+        if (col.tag == "Player")
+        {
+            // Load the Next Level
+            Application.LoadLevel(Application.loadedLevel + 1);
+        }
+
+    }
+
+}
